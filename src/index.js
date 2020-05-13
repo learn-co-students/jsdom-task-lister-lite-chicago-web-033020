e1e068de2be70a4f1bef4f0f3656cbd8ffaaf783
jguz17 <johnathanguzman17@gmail.com>
2020-05-13 00:58:06 -0500

    added delete button

--- src/index.js
+++ src/index.js
@@ -16,9 +16,21 @@ document.querySelector("#create-task-form").addEventListener("submit", function(
   // --- create li node
   const createListItem = document.createElement("li");
 
+  // -- create delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+
+  // --- add event listener on button to delete li  
+  deleteButton.addEventListener("click",function(event){
+    event.target.parentNode.remove()
+  })
+  
+  // --- add delete button to li's
+  createListItem.append(deleteButton);
+  
   // --- add li to list, add content to li
   getList.appendChild(createListItem).append(taskContent);
 
-  // --- resets text field
+  // --- reset text field
   document.getElementById("create-task-form").reset();
 });
